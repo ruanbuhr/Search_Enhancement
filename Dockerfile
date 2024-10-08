235a1481fa385039a92d50068f0fc3895cfab57e
Ruan Hermann Buhr <26440873@sun.ac.za>
2024-10-08 13:52:11 +0200

    Containerize the application
    The flask application is ready for deployment

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM python:3.12-slim
+
+# realtime log updates
+ENV PYTHONUNBUFFERED True
+
+WORKDIR /app
+COPY . ./
+
+RUN pip install --no-cache-dir --upgrade pip
+RUN pip install --no-cache-dir -r requirements.txt
+
+CMD exec gunicorn --bind :$PORT --workers 1 --threads 8 --timeout 0 main:app
--- main.py
+++ main.py
@@ -1,20 +1,32 @@
 from flask import Flask, request, jsonify
 import os
 from dotenv import load_dotenv
+from flask_cors import CORS
 from google_search import *
 
 load_dotenv()
 
 app = Flask(__name__)
 
-port = int(os.getenv('API_PORT', 5000))
+# Accept cross origin requests
+CORS(app)
+
+port = int(os.getenv('PORT', 8080))
 
 @app.route('/search', methods=['POST'])
 def search():
     data = request.json
+
+    if data is None:
+        return jsonify({ "error": "No JSON received."}), 400
+
     query = data.get('query')
+
+    if query is None:
+        return jsonify({"error": "Query not found."}), 400
+
     results = google_search(query)
     return jsonify(results)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
